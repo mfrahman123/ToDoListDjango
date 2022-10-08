8cce1b3cc85fe708436e9765fe823aaf9a793d96
mfrahman123 <99muhammad.r@gmail.com>
2022-10-08 20:55:22 +0100

    initial commit

---
+++ src/project/base/urls.py
@@ -0,0 +1,13 @@
+from turtle import Pen
+from django.urls import path
+from .views import PendingList, TaskDetail,CreateTask,EditTask,DeleteTask, Login,RegisterPage
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [path('', PendingList.as_view(), name='tasks'),
+path('login/', Login.as_view(), name='login'),
+path('register/', RegisterPage.as_view(), name='register'),
+path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+path('create-task/', CreateTask.as_view(), name='create-task'),
+path('edit-task/<int:pk>', EditTask.as_view(), name='edit-task'),
+path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task')]
